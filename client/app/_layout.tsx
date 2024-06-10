85479b80e74b6058ad9aaff9e9a77cc3286f0cc6
Long Dao <hailongdao1017@gmail.com>
2024-06-10 15:16:24 -0700

    uhih

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -27,13 +27,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    // <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name='index' options={{ headerShown: false }} />
         <Stack.Screen name="auth" options={{ headerShown: false }} />
         <Stack.Screen name='unauth' options={{ headerShown: false }} />
 
       </Stack>
-    </ThemeProvider>
+    // </ThemeProvider>
   );
 }
