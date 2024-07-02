3a2d86865a574a4e9b5bd0878d7124d5ac595ab0
Nhat Nguyen <102700368+lasksafj@users.noreply.github.com>
2024-07-02 14:50:02 -0700

    add logger

--- server_nodejs/src/index.js
+++ server_nodejs/src/index.js
@@ -3,39 +3,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const logger = require('./utils/logger');
+const requestLogger = require('./middlewares/loggerMiddleware');
+const userRoutes = require('./routes/userRoutes');
+
 
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-
-// Middleware to log incoming requests and outgoing responses
-const requestLogger = (req, res, next) => {
-    const originalSend = res.send;
-    let responseBody = '';
-
-    res.send = function (body) {
-        responseBody = body;
-        return originalSend.apply(this, arguments);
-    };
-
-    res.on('finish', () => {
-        if (res.statusCode >= 400) {
-            logger.error(`${new Date().toLocaleString()} - ${req.method} ${req.url} - Status: ${res.statusCode} - Response: ${responseBody}`);
-        }
-        else {
-            logger.info(`${new Date().toLocaleString()} - ${req.method} ${req.url} - Status: ${res.statusCode}`);
-        }
-    });
-
-    next();
-};
 app.use(requestLogger);
 
 // Routes
-const userRoutes = require('./routes/userRoutes');
 app.use('/api/user', userRoutes);
 
 // Start the server
---
+++ server_nodejs/src/middlewares/loggerMiddleware.js
@@ -0,0 +1,24 @@
+const logger = require('../utils/logger');
+
+// Middleware to log incoming requests and outgoing responses
+const requestLogger = (req, res, next) => {
+    const originalSend = res.send;
+    let responseBody = '';
+
+    res.send = function (body) {
+        responseBody = body;
+        return originalSend.apply(this, arguments);
+    };
+
+    res.on('finish', () => {
+        if (res.statusCode >= 400) {
+            logger.error(`${new Date().toLocaleString()} - ${req.method} ${req.url} - Status: ${res.statusCode} - Response: ${responseBody}`);
+        }
+        else {
+            logger.info(`${new Date().toLocaleString()} - ${req.method} ${req.url} - Status: ${res.statusCode}`);
+        }
+    });
+
+    next();
+};
+module.exports = requestLogger;
