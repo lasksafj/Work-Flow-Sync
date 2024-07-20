a5f1e6dd81b69f493065091b6a8eb99111d10673
RhumTea <160001872+rhumtea@users.noreply.github.com>
2024-07-19 21:45:53 -0700

    Update profileGetOrg
    add select abbrevation, address

--- server_nodejs/src/controllers/profileController.js
+++ server_nodejs/src/controllers/profileController.js
@@ -21,7 +21,7 @@ exports.profileGetOrg = async (req, res) => {
     console.log(req.user);
     try {
         const data = await db.query(
-            `select o.name
+            `select o.abbreviation as abbreviation, o.name as name, o.address as address
             from employees e
             join organizations o ON e.org_abbreviation = o.abbreviation
             where e.user_id=$1;`,
