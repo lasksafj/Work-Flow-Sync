785f045d1e44ae5865cb8213981a1942f2bbbfc5
RhumTea <160001872+rhumtea@users.noreply.github.com>
2024-08-09 18:33:51 -0700

    Update

--- server_nodejs/src/index.js
+++ server_nodejs/src/index.js
@@ -8,14 +8,11 @@ const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const earningsRoutes = require('./routes/earningsRoutes'); // Anh
 const notificationsRoutes = require('./routes/notificationsRoutes'); // Long
+const profileRoutes = require("./routes/profileRoutes");
 
 const socketConfig = require('./config/socket');
 const authSocketMiddleware = require('./middlewares/authSocketMiddleware');
 const chatSocket = require('./socket/chatSocket');
-
-const socketConfig = require("./config/socket");
-const authSocketMiddleware = require("./middlewares/authSocketMiddleware");
-const chatSocket = require("./socket/chatSocket");
 
 const PORT = process.env.PORT || 3000;
 
@@ -29,6 +26,7 @@ app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/earnings', earningsRoutes);
 app.use('/api/notifications', notificationsRoutes);
+app.use("/api/profile", profileRoutes);
 
 
 // Start the server
