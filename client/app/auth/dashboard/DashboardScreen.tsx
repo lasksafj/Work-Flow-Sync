f9a959329135d48849c1d4990a706e2129e6aa52
Quy Luyt Tran <102700644+luvidaa@users.noreply.github.com>
2024-09-17 12:50:05 -0700

    update (dashboard Screen)
    refresh page, modify date format

--- client/app/auth/dashboard/DashboardScreen.tsx
+++ client/app/auth/dashboard/DashboardScreen.tsx
@@ -1,12 +1,17 @@
 import api from "@/apis/api";
 import { useAppSelector } from "@/store/hooks";
 import { RootState } from "@/store/store";
-import { format } from "date-fns";
-import moment from "moment";
 import React, { useEffect, useMemo, useState } from "react";
-import { StyleSheet, View, TouchableWithoutFeedback, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+    StyleSheet,
+    View,
+    TouchableWithoutFeedback,
+    Text,
+    ScrollView,
+    RefreshControl,
+} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+import moment from "moment";
 
 const tabs = [
     { name: "Mon" },
@@ -52,34 +57,39 @@ export default function DashboardScreen() {
     const user = useAppSelector((state: RootState) => state.user);
     const organization = useAppSelector((state: RootState) => state.organization);
 
-    const [selectedIndex, setSelectedIndex] = React.useState((new Date().getDay() - 1 + 7) % 7);
-    // const [selectedIndex, setSelectedIndex] = React.useState(new Date().getDay() - 1);
+    // Adjust selectedIndex to match the week starting on Monday
+    const [selectedIndex, setSelectedIndex] = React.useState((new Date().getDay() + 6) % 7);
     const [shiftDetail, setShiftDetail] = useState(sampleDetails);
+    const [refreshing, setRefreshing] = useState(false);
 
-    const selectedDetails = sampleDetails[selectedIndex];
+    const selectedDetails = shiftDetail[selectedIndex];
 
+    // Generate daysOfWeek starting from Monday
     const daysOfWeek = useMemo(
         () =>
             Array.from({ length: 7 }, (v, i) =>
-                moment().startOf("week").add(i, "days").format("YYYY-MM-DD")
+                moment().startOf("isoWeek").add(i, "days").format("YYYY-MM-DD")
             ),
         []
     );
 
-    useEffect(() => {
-        api.get(`/api/dashboard/get-detail-shift?orgAbbr=${organization.abbreviation}`).then(
-            (res) => {
+    const fetchShiftDetails = () => {
+        api.get(`/api/dashboard/get-detail-shift?orgAbbr=${organization.abbreviation}`)
+            .then((res) => {
                 const data = res.data;
-                let newShiftDetail = sampleDetails;
-                // console.log("------------------", data);
+                let newShiftDetail = sampleDetails.map((detail) => ({
+                    ...detail,
+                }));
+
+                // Set the dates for the shift details
                 for (let i = 0; i < 7; i++) {
-                    newShiftDetail[i].date = daysOfWeek[(i + 1) % 7];
+                    newShiftDetail[i].date = daysOfWeek[i];
                 }
 
-                data.map((d: any) => {
+                data.forEach((d: any) => {
+                    const shiftDate = moment(d.start_time).format("YYYY-MM-DD");
                     for (let i = 0; i < 7; i++) {
-                        let start_date = new Date(d.start_time);
-                        if (moment(start_date).format("YYYY-MM-DD") == newShiftDetail[i].date) {
+                        if (shiftDate === newShiftDetail[i].date) {
                             newShiftDetail[i].shiftStart = d.start_time;
                             newShiftDetail[i].shiftEnd = d.end_time;
                             newShiftDetail[i].location = d.organization_name;
@@ -88,99 +98,123 @@ export default function DashboardScreen() {
                     }
                 });
 
-                // console.log("++++++++++++++++", newShiftDetail);
-
                 setShiftDetail(newShiftDetail);
-            }
-        );
+                setRefreshing(false);
+            })
+            .catch((error) => {
+                console.error("Error fetching shift details:", error);
+                setRefreshing(false);
+            });
+    };
+
+    useEffect(() => {
+        fetchShiftDetails();
     }, []);
+
+    const onRefresh = () => {
+        setRefreshing(true);
+        fetchShiftDetails();
+    };
 
     return (
         <View style={{ flex: 1, backgroundColor: "white" }}>
-            <Text style={styles.textHeader}>Your upcoming schedules:</Text>
+            <ScrollView
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                        tintColor="#4338ca" // Spinner color for iOS
+                        title="Loading..." // Text below the spinner
+                        titleColor="#4338ca" // Text color
+                        colors={["#4338ca"]} // Spinner color for Android
+                        progressBackgroundColor="#e0e7ff" // Background color of the spinner
+                    />
+                }
+                contentContainerStyle={{ flexGrow: 1 }}
+            >
+                <Text style={styles.textHeader}>Your upcoming schedules:</Text>
 
-            <View style={styles.container}>
-                {tabs.map((item, index) => {
-                    const isActive = index === selectedIndex;
+                <View style={styles.container}>
+                    {tabs.map((item, index) => {
+                        const isActive = index === selectedIndex;
 
-                    return (
-                        <View key={item.name} style={{ flex: 1 }}>
-                            <TouchableWithoutFeedback
-                                onPress={() => {
-                                    setSelectedIndex(index);
-                                }}
-                            >
-                                <View
-                                    style={[
-                                        styles.item,
-                                        isActive && { backgroundColor: "#e0e7ff" },
-                                    ]}
+                        return (
+                            <View key={item.name} style={{ flex: 1 }}>
+                                <TouchableWithoutFeedback
+                                    onPress={() => {
+                                        setSelectedIndex(index);
+                                    }}
                                 >
-                                    <Text style={[styles.text, isActive && { color: "#4338ca" }]}>
-                                        {item.name}
+                                    <View
+                                        style={[
+                                            styles.item,
+                                            isActive && { backgroundColor: "#e0e7ff" },
+                                        ]}
+                                    >
+                                        <Text
+                                            style={[styles.text, isActive && { color: "#4338ca" }]}
+                                        >
+                                            {item.name}
+                                        </Text>
+                                    </View>
+                                </TouchableWithoutFeedback>
+                            </View>
+                        );
+                    })}
+                </View>
+
+                <View style={styles.detailsWrapper}>
+                    <View style={styles.detailsContainer}>
+                        <View style={styles.detailRow}>
+                            <Icon name="calendar" size={20} color="#4b5563" />
+                            <Text style={styles.detailsText}>
+                                {moment(selectedDetails.date, "YYYY-MM-DD").format("DD/MM/YYYY")}
+                            </Text>
+                        </View>
+                        <View style={styles.detailRow}>
+                            <Icon name="clock-o" size={20} color="#4b5563" />
+                            <Text style={styles.detailsText}>
+                                {selectedDetails.shiftStart !== "N/A"
+                                    ? `${formatTime(selectedDetails.shiftStart)} - ${formatTime(
+                                          selectedDetails.shiftEnd
+                                      )}`
+                                    : "N/A"}
+                            </Text>
+                        </View>
+                        <View style={styles.detailRow}>
+                            <Icon name="user" size={20} color="#4b5563" />
+                            <Text style={styles.detailsText}>{selectedDetails.role}</Text>
+                        </View>
+                        <View style={styles.detailRow}>
+                            <Icon name="map-marker" size={20} color="#4b5563" />
+                            <Text style={styles.detailsText}>{selectedDetails.location}</Text>
+                        </View>
+                        <View style={styles.sectionDivider} />
+                        <Text style={styles.sectionHeaderText}>Upcoming event:</Text>
+                        {selectedDetails.upcomingEvent ? (
+                            <>
+                                <View style={styles.detailRow}>
+                                    <Icon name="clock-o" size={20} color="#4b5563" />
+                                    <Text style={styles.detailsText}>
+                                        {selectedDetails.upcomingEvent.time}
                                     </Text>
                                 </View>
-                            </TouchableWithoutFeedback>
-                        </View>
-                    );
-                })}
-            </View>
-
-            <View style={styles.detailsWrapper}>
-                <View style={styles.detailsContainer}>
-                    <View style={styles.detailRow}>
-                        <Icon name="calendar" size={20} color="#4b5563" />
-                        <Text style={styles.detailsText}>
-                            {/* {format(selectedDetails.date, "MM/dd/yyyy")} */}
-                            {selectedDetails.date}
-                        </Text>
-                    </View>
-                    <View style={styles.detailRow}>
-                        <Icon name="clock-o" size={20} color="#4b5563" />
-                        <Text style={styles.detailsText}>
-                            {selectedDetails.shiftStart != "N/A"
-                                ? `${format(selectedDetails.shiftStart, "hh:mm a")} - ${format(
-                                      selectedDetails.shiftEnd,
-                                      "hh:mm a"
-                                  )}`
-                                : "N/A"}
-                        </Text>
-                    </View>
-                    <View style={styles.detailRow}>
-                        <Icon name="user" size={20} color="#4b5563" />
-                        <Text style={styles.detailsText}>{selectedDetails.role}</Text>
-                    </View>
-                    <View style={styles.detailRow}>
-                        <Icon name="map-marker" size={20} color="#4b5563" />
-                        <Text style={styles.detailsText}>{selectedDetails.location}</Text>
-                    </View>
-                    <View style={styles.sectionDivider} />
-                    <Text style={styles.sectionHeaderText}>Upcoming event:</Text>
-                    {selectedDetails.upcomingEvent ? (
-                        <>
-                            <View style={styles.detailRow}>
-                                <Icon name="clock-o" size={20} color="#4b5563" />
-                                <Text style={styles.detailsText}>
-                                    {selectedDetails.upcomingEvent.time}
-                                </Text>
-                            </View>
-                            <View style={styles.detailRow}>
-                                <Icon name="users" size={20} color="#4b5563" />
-                                <Text style={styles.detailsText}>
-                                    {selectedDetails.upcomingEvent.attendees}
-                                </Text>
-                            </View>
-                        </>
-                    ) : (
-                        <>
+                                <View style={styles.detailRow}>
+                                    <Icon name="users" size={20} color="#4b5563" />
+                                    <Text style={styles.detailsText}>
+                                        {selectedDetails.upcomingEvent.attendees}
+                                    </Text>
+                                </View>
+                            </>
+                        ) : (
                             <View style={styles.detailRow}>
                                 <Icon name="calendar-times-o" size={20} color="#4b5563" />
                                 <Text style={styles.detailsText}>No upcoming events</Text>
                             </View>
-                        </>
-                    )}
+                        )}
+                    </View>
                 </View>
-            </View>
+            </ScrollView>
         </View>
     );
 }
